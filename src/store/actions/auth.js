16698c3743b4808363e48bda060f4c9c67c68421
Vyacheslav Serdyuk <Navarachkou@gmail.com>
2019-08-10 16:00:36 +0500

    Add lazy-loading and refactor utility fnc

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -62,7 +62,7 @@ export const auth = (email, password, isSignup) => {
         dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch(err => {
-        dispatch(authFail(err.response.data.error))
+        dispatch(authFail(err));
       })
   }
 }
